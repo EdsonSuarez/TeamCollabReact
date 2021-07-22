5bae7b458218a29a3a149dc8ab84c0f680ca186a
miguel199627 <cerquera199627@hotmail.com>
2021-07-22 11:29:45 -0500

    Styles project

--- frontend/src/components/Project/ProjectBox/ProjectBoxList/index.js
+++ frontend/src/components/Project/ProjectBox/ProjectBoxList/index.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faChalkboardTeacher, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  Link
+} from "react-router-dom";
+import moment from 'moment';
 
 export default function ProjectBoxList({
   _id,
@@ -25,22 +29,22 @@ export default function ProjectBoxList({
   };
 
   return (
-    <div className="card m-2" style={{ width: "18rem" }}>
+    <div className={`card m-2 ${active ? 'card-success' : 'card-secondary'}`} style={{ width: "18rem" }}>
       <div className="card-header">{name}</div>
       <div className="card-body">
         <p className="card-text text-justify">{description}</p>
         <div className="row">
           <h5 className="card-title text-start">
             Status:
-            <span>{status}</span>
+            <span className={`badge text-dark ${status === 'done' ? 'bg-success' : status === 'doing' ? 'bg-warning' : 'bg-danger'}`}>{status}</span>
           </h5>
         </div>
         <div className="row">
-          <h5 className="card-title text-start">Date: {date}</h5>
+          <h5 className="card-title text-start">Date: {moment(date).format('DD-MM-yyyy')}</h5>
         </div>
         <div className="row">
           <h5 className="card-title text-start">
-            <span className="badge text-dark">
+            <span className={`badge text-dark ${active ? 'bg-success' : 'bg-warning'}`}>
               {active ? "Active" : "Deleted"}
             </span>
           </h5>
@@ -63,6 +67,9 @@ export default function ProjectBoxList({
         >
           <FontAwesomeIcon icon={faTrash} />
         </button>
+        <Link to={`/board/${_id}`} className="btn btn-info btn-xs m-1">
+          <FontAwesomeIcon icon={faChalkboardTeacher}/>
+        </Link>
       </div>
     </div>
   );
