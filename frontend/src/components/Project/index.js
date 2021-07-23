28bf988d7ad1d0125122299fcee0c7d55e12d8e3
miguel199627 <cerquera199627@hotmail.com>
2021-07-23 10:56:10 -0500

    Add alerts

--- frontend/src/components/Project/index.js
+++ frontend/src/components/Project/index.js
@@ -1,9 +1,18 @@
 import { useState, useEffect } from "react";
-import { fetchAdmin, fetchScrum, fetchUserLeader } from "../../services/project";
+import {
+  fetchAdmin,
+  fetchScrum,
+  fetchUserLeader,
+} from "../../services/project";
 import { isAdmin, isScrumMaster, isUserLeader } from "../../services/auth";
-import { saveProject, updateProject, deleteProject } from "../../services/project";
+import {
+  saveProject,
+  updateProject,
+  deleteProject,
+} from "../../services/project";
 
 import ProjectBox from "./ProjectBox";
+import Alerts from "./Alerts";
 
 export default function Project() {
   const objForm = {
@@ -14,20 +23,24 @@ export default function Project() {
     description: "",
     status: "",
     date: "",
-    active: ""
+    active: "",
   };
   const [projects, setProjects] = useState();
   const [projectsSearch, setProjectsSearch] = useState();
   const [showForm, setShowForm] = useState(objForm);
-  const [message, setMessage] = useState();
+  const [alertDetail, setAlertDetail] = useState({
+    show: false,
+    type: "",
+    message: "",
+  });
 
   const inicio = () => {
     if (isAdmin()) listAdmin();
-    if(isScrumMaster()) listScrum();
-    if(isUserLeader()) listUserLeader();
-    if(localStorage.getItem('sprint') && localStorage.getItem('team')) {
-      localStorage.removeItem('team');
-      localStorage.removeItem('sprint');
+    if (isScrumMaster()) listScrum();
+    if (isUserLeader()) listUserLeader();
+    if (localStorage.getItem("sprint") && localStorage.getItem("team")) {
+      localStorage.removeItem("team");
+      localStorage.removeItem("sprint");
     }
   };
 
@@ -48,7 +61,13 @@ export default function Project() {
   };
 
   const listUserLeader = () => {
-    fetchUserLeader().then(({ data: { projects } }) => {
+    fetchUserLeader().then(({ data: { team } }) => {
+      let projects = [];
+      team.map(({ teamId }) => {
+        if (teamId !== null) {
+          projects.push(teamId.projectId);
+        }
+      });
       setShowForm(objForm);
       setProjects(projects);
       setProjectsSearch(projects);
@@ -56,15 +75,19 @@ export default function Project() {
   };
 
   const handleProjectBoxFormSave = (dataSave) => {
-    if(dataSave._id === '') {
+    if (dataSave._id === "") {
       saveProject(dataSave).then(({ data: { result } }) => {
         setShowForm(objForm);
         setProjects([...projects, result]);
+        alertCtrl("success", "Project created with success!");
       });
     } else {
       updateProject(dataSave).then(({ data: { project } }) => {
         setShowForm(objForm);
-        setProjects(projects.map((item) => (item._id === project._id ? project : item)));
+        setProjects(
+          projects.map((item) => (item._id === project._id ? project : item))
+        );
+        alertCtrl("success", "Project updated with success!");
       });
     }
   };
@@ -79,7 +102,14 @@ export default function Project() {
     setShowForm(objForm);
   };
 
-  const handleProjectBoxListEdit = (_id, name, description, status, date, active) => {
+  const handleProjectBoxListEdit = (
+    _id,
+    name,
+    description,
+    status,
+    date,
+    active
+  ) => {
     objForm._id = _id;
     objForm.show = true;
     objForm.title = `Edit project: ${name}`;
@@ -94,27 +124,51 @@ export default function Project() {
 
   const handleProjectBoxListDelete = (dataDelete) => {
     deleteProject(dataDelete).then(({ data: { project } }) => {
-      setProjects(projects.map((item) => (item._id === project._id ? project : item)));
+      setProjects(
+        projects.map((item) => (item._id === project._id ? project : item))
+      );
+      alertCtrl("success", "Project deleted with success!");
     });
   };
 
   const handleProjectBoxSearch = (search) => {
     search = search.trim().toLowerCase();
-    setProjects(search === '' ? projectsSearch : projects.filter((value) => value.name.toLowerCase().indexOf(search) > -1));
+    setProjects(
+      search === ""
+        ? projectsSearch
+        : projects.filter(
+            (value) => value.name.toLowerCase().indexOf(search) > -1
+          )
+    );
+  };
+
+  const alertCtrl = (type, message) => {
+    setAlertDetail({
+      ...alertDetail,
+      show: true,
+      type: type,
+      message: message,
+    });
+    setTimeout(() => {
+      setAlertDetail({ ...alertDetail, show: false });
+    }, 2000);
   };
 
   useEffect(() => inicio(), []);
 
   return (
-    <ProjectBox
-      projects={projects}
-      onProjectBoxSave={handleProjectBoxFormSave}
-      objForm={showForm}
-      onProjectBoxClose={handleProjectBoxFormClose}
-      onProjectBoxNew={handleProjectBoxNew}
-      onProjectBoxEdit={handleProjectBoxListEdit}
-      onProjectBoxDelete={handleProjectBoxListDelete}
-      onProjectBoxSearch={handleProjectBoxSearch}
-    />
+    <>
+      <Alerts {...alertDetail} />
+      <ProjectBox
+        projects={projects}
+        onProjectBoxSave={handleProjectBoxFormSave}
+        objForm={showForm}
+        onProjectBoxClose={handleProjectBoxFormClose}
+        onProjectBoxNew={handleProjectBoxNew}
+        onProjectBoxEdit={handleProjectBoxListEdit}
+        onProjectBoxDelete={handleProjectBoxListDelete}
+        onProjectBoxSearch={handleProjectBoxSearch}
+      />
+    </>
   );
 }
